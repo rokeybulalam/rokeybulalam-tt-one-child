b78a8a2c86b868ba80e14c22515afc2f89faa6f9
rokeybulalam <rokeybulalam1122@gmail.com>
2023-07-25 22:00:07 +0600

    module 9.7

--- elementor-addons/widgets/hello-world.php
+++ elementor-addons/widgets/hello-world.php
@@ -22,10 +22,31 @@ class Rokeybulalam_Elementor_Addon_Hello_World extends \Elementor\Widget_Base {
 		return [ 'hello', 'world' ];
 	}
 
+	protected function register_controls(){
+	$this->start_controls_section(
+		'content',
+		[
+			'label' => esc_html__( 'content', 'elementor-addon' ),
+			'tab' => \Elementor\Controls_Manager::TEXT,
+		]
+	);
+
+	$this->add_control(
+		'title',
+		[
+			'label' => esc_html__( 'Title', 'elementor-addon' ),
+			'type' => \Elementor\Controls_Manager::TEXTAREA,
+			'default' => esc_html__( 'Hello world', 'elementor-addon' ),
+		]
+	);
+
+	$this->end_controls_section();
+	}
 	protected function render() {
+		$settings = $this->get_settings_for_display();
 		?>
 
-		<p> Hello World </p>
+		<p><?php echo $settings['title'];?></p>
 
 		<?php
 	}
